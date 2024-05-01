c77c6804d206f69b7dc2f504c40a139ba1cd8492
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-01 13:25:49 +0300

    initial commit

--- my_greenhouse/lib/main.dart
+++ my_greenhouse/lib/main.dart
@@ -2,19 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:my_greenhouse/UI/onboardingScreen.dart';
 
 void main(){
+  // ignore: prefer_const_constructors
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp(Key? key) : super(key: key);
-  
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'onboarding screen',
+      title: 'onboarding Screen',
       home: OnboardingScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
-  
 }
+  
