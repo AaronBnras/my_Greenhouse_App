c8121ba2a5af5c33ebd92c963ef48459c7c77ff2
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-08 03:54:39 +0300

    initial commit

--- my_greenhouse/lib/UI/screens/Sign_in.dart
+++ my_greenhouse/lib/UI/screens/Sign_in.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:my_greenhouse/UI/root-page.dart';
+import 'package:my_greenhouse/UI/screens/sign_up.dart';
 import 'package:my_greenhouse/constants.dart';
 import 'package:my_greenhouse/UI/screens/widgets/custom_textfield.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'forgot_password.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
@@ -10,117 +14,155 @@ class SignIn extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset('assets/images/signin.png'),
-            const Text(
-              'Sign In',
-              style: TextStyle(
-                fontSize: 35.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const customtextfield(
-              obscureText: false,
-              hintText: 'Enter Email',
-              icon: Icons.alternate_email,
-            ),
-            const customtextfield(
-              obscureText: true,
-              hintText: 'Enter Password',
-              icon: Icons.lock,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const rootpage()));
-              },
-              child: Container(
-                width: size.width,
-                decoration: BoxDecoration(
-                  color: Constants.primaryColor,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: const Center(
-                  child: Text(
+    return Material(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  child: ListView(
+                children: [
+                  Image.asset('assets/images/signin.png'),
+                  const Text(
                     'Sign In',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            GestureDetector(
-              onTap: () {
-                // Navigator.pushReplacement(
-                //     context,
-                //     PageTransition(
-                //         child: const ForgotPassword(),
-                //         type: PageTransitionType.bottomToTop));
-              },
-              child: Center(
-                child: Text.rich(
-                  TextSpan(children: [
-                    TextSpan(
-                      text: 'Forgot Password? ',
-                      style: TextStyle(
-                        color: Constants.blackColor,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Reset Here',
-                      style: TextStyle(
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const customtextfield(
+                    obscureText: false,
+                    hintText: 'Enter Email',
+                    icon: Icons.alternate_email,
+                  ),
+                  const customtextfield(
+                    obscureText: true,
+                    hintText: 'Enter Password',
+                    icon: Icons.lock,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (_) => const rootpage()));
+                    },
+                    child: Container(
+                      width: size.width,
+                      decoration: BoxDecoration(
                         color: Constants.primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 20),
+                      child: const Center(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          ),
+                        ),
                       ),
                     ),
-                  ]),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const rootpage()));
-              },
-              child: Container(
-                width: size.width,
-                decoration: BoxDecoration(
-                  color: Constants.primaryColor,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding:
-                const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: const Center(
-                  child: Text(
-                    'Sign In',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                          context,
+                          PageTransition(
+                              child: const ForgotPassword(),
+                              type: PageTransitionType.bottomToTop));
+                    },
+                    child: Center(
+                      child: Text.rich(
+                        TextSpan(children: [
+                          TextSpan(
+                            text: 'Forgot Password? ',
+                            style: TextStyle(
+                              color: Constants.blackColor,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Reset Here',
+                            style: TextStyle(
+                              color: Constants.primaryColor,
+                            ),
+                          ),
+                        ]),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            )
-          ],
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Row(
+                    children: [
+                      Expanded(child: Divider()),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text('OR'),
+                      ),
+                      Expanded(child: Divider()),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: size.width,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Constants.primaryColor),
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          height: 30,
+                          child: Image.asset('../assets/Images/google.png'),
+                        ),
+                        Text(
+                          'Sign In with Google',
+                          style: TextStyle(
+                              color: Constants.blackColor, fontSize: 18.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.pushReplacement(context, PageTransition(
+                        child: const SignUp(),
+                        type: PageTransitionType.bottomToTop
+                      ));
+                    },
+                    child: Center(
+                      child: Text.rich(TextSpan(
+                        children: [
+
+                        ]
+                      )),
+                    ),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
