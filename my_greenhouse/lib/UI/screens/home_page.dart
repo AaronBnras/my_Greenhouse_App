7f6fa332d06f3f5da5899f455ab31fd0f309b2e3
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-12 00:00:49 +0300

    Initial updates

--- my_greenhouse/lib/UI/screens/home_page.dart
+++ my_greenhouse/lib/UI/screens/home_page.dart
@@ -15,77 +15,81 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: Constants.horizontalPadding,
-              vertical: Constants.verticalPadding),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-                elevation: 10,
-                child: Container(
-                  color: Constants.cardColor.withOpacity(0.2),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: Constants.horizontalPadding,
-                        vertical: Constants.verticalPadding),
-                    child: Row(
-                      children: [
-                        AnimatedTextKit(
-                          animatedTexts: [
-                            TypewriterAnimatedText('Hello "Username"',
-                                textStyle: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                speed: const Duration(milliseconds: 100))
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: Constants.horizontalPadding,
+                  vertical: Constants.verticalPadding),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    elevation: 10,
+                    child: Container(
+                      color: Constants.cardColor.withOpacity(0.2),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: Constants.horizontalPadding,
+                            vertical: Constants.verticalPadding),
+                        child: Row(
+                          children: [
+                            AnimatedTextKit(
+                              animatedTexts: [
+                                TypewriterAnimatedText('Hello "Username"',
+                                    textStyle: const TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                    speed: const Duration(milliseconds: 100))
+                              ],
+                              totalRepeatCount: 4,
+                              pause: const Duration(milliseconds: 1000),
+                              displayFullTextOnTap: true,
+                              stopPauseOnTap: true,
+                            )
                           ],
-                          totalRepeatCount: 4,
-                          pause: const Duration(milliseconds: 1000),
-                          displayFullTextOnTap: true,
-                          stopPauseOnTap: true,
-                        )
-                      ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: Constants.horizontalPadding,
+                        vertical: 5.0),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Welcome'),
+                                Text('Aaron Maeda',
+                                  style: TextStyle(
+                                      fontSize: 24.0,
+                                      fontWeight: FontWeight.w900
+                                  ),
+                                ),
+                                Text('monitor our greenhouse farm')
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ) ,
+                  )
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: Constants.horizontalPadding,
-                    vertical: Constants.verticalPadding),
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                   Row(
-                     children: [
-                       Column(
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: [
-                           Text('Welcome'),
-                           Text('Aaron Maeda',
-                             style: TextStyle(
-                               fontSize: 24.0,
-                               fontWeight: FontWeight.w900
-                             ),
-                           ),
-                           Text('monitor our greenhouse farm')
-                         ],
-                       )
-                     ],
-                   )
-                  ],
-                ) ,
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
