dd87bef0c9b1f1ea5d3d8867cff0a05920ea0c61
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-17 17:32:14 +0300

    Initial commit

---
+++ my_greenhouse/lib/models/temperature._box.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class TemperatureBox extends StatefulWidget {
+  const TemperatureBox({super.key});
+
+  @override
+  State<TemperatureBox> createState() => _TemperatureBoxState();
+}
+
+class _TemperatureBoxState extends State<TemperatureBox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
