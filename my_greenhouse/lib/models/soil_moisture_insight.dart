2174e2c049ac584fc44d44937baa863c47ecd855
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-18 23:41:34 +0300

    Initial update

---
+++ my_greenhouse/lib/models/soil_moisture_insight.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SoilMoistureInsight extends StatefulWidget {
+  const SoilMoistureInsight({super.key});
+
+  @override
+  State<SoilMoistureInsight> createState() => _SoilMoistureInsightState();
+}
+
+class _SoilMoistureInsightState extends State<SoilMoistureInsight> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ my_greenhouse/lib/models/water_level_insight.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class WaterLevelInsight extends StatefulWidget {
+  const WaterLevelInsight({super.key});
+
+  @override
+  State<WaterLevelInsight> createState() => _WaterLevelInsightState();
+}
+
+class _WaterLevelInsightState extends State<WaterLevelInsight> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
