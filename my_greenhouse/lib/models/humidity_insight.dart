f86b5499f45e4e88f15ac6705fe5b02460d187f8
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-18 23:37:17 +0300

    Initial Commit

---
+++ my_greenhouse/lib/models/humidity_insight.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HumidityInsight extends StatefulWidget {
+  const HumidityInsight({super.key});
+
+  @override
+  State<HumidityInsight> createState() => _HumidityInsightState();
+}
+
+class _HumidityInsightState extends State<HumidityInsight> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
