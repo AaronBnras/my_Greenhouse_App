3f67c5176bc42f3445275c9579601a32ce26e554
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-18 23:40:19 +0300

    Initial update

--- my_greenhouse/lib/models/temperature._insight.dart
+++ my_greenhouse/lib/models/temperature._insight.dart
@@ -9,60 +9,50 @@ class TemperatureInsight extends StatefulWidget {
 }
 
 class _TemperatureInsightState extends State<TemperatureInsight> {
-  // double _scale = 1.0;
-  //
-  // void _handleTap() {
-  //   setState(() {
-  //     _scale = 0.8; // Scale down on tap
-  //   });
-  //   Future.delayed(const Duration(milliseconds: 200), () {
-  //     setState(() {
-  //       _scale = 1.0; // Scale back to original
-  //     });
-  //     Navigator.pushReplacement(context, PageTransition(child: const RootPage(), type: PageTransitionType.bottomToTop));
-  //   });
-  // }
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: Constants.horizontalPadding,
-                  vertical: Constants.verticalPadding),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Temperature',
-                    style: TextStyle(
-                      color: Constants.primaryColor,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 24,
-                    ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 15),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Temperature',
+                  style: TextStyle(
+                    color: Constants.primaryColor,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
                   ),
-                ],
-              )),
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          scrolledUnderElevation: 0.0,
-        ),
-        body: Column(
-          children: [
-            InkWell(
-              onTap: () {
-                Navigator.pop(context as BuildContext);
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Tap here to go back',
-                  style: TextStyle(fontSize: 20, color: Colors.blue),
                 ),
-              ),
+              ],
             ),
-          ],
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            scrolledUnderElevation: 0.0,
+          ),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: Constants.horizontalPadding, vertical: Constants.verticalPadding),
+            child: Row(
+              children: [
+                Container(
+                  color: Colors.blue,
+                  child: const Column(
+                    children: [
+                      Row(
+                        children: [
+
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ),
       ),
     );
