0b018e8fb3ee1cc5c7dffe60702809bee80e3240
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-05-19 00:15:06 +0300

    Initial update

--- my_greenhouse/lib/UI/screens/Profile.dart
+++ my_greenhouse/lib/UI/screens/Profile.dart
@@ -1,22 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:my_greenhouse/UI/screens/widgets/profile_widget.dart';
+import 'package:my_greenhouse/constants.dart';
 
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
 
-  @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Scaffold(
-          body: Center(
-            child: Text('Profile'),
-          )
-      ),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            height: size.height,
+            width: size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Constants.primaryColor.withOpacity(.5),
+                      width: 5.0,
+                    ),
+                  ),
+                  child: const CircleAvatar(
+                    radius: 60,
+                    backgroundImage: ExactAssetImage('assets/images/profile.jpg'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: size.width * .3,
+                  child: Row(
+                    children: [
+                      Text(
+                        'John Doe',
+                        style: TextStyle(
+                          color: Constants.blackColor,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                          height: 24,
+                          child: Image.asset("assets/images/verified.png")),
+                    ],
+                  ),
+                ),
+                Text(
+                  'johndoe@gmail.com',
+                  style: TextStyle(
+                    color: Constants.blackColor.withOpacity(.3),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: size.height * .7,
+                  width: size.width,
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ProfileWidget(
+                        icon: Icons.person,
+                        title: 'My Profile',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.settings,
+                        title: 'Settings',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.notifications,
+                        title: 'Notifications',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.chat,
+                        title: 'FAQs',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.share,
+                        title: 'Share',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.logout,
+                        title: 'Log Out',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
     );
   }
 }
-
