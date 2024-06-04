91dd749fad49d9f9bb3713d9a5838bf0446212cc
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-06-04 05:51:20 +0300

    Initial update

--- my_greenhouse/lib/UI/screens/home_page.dart
+++ my_greenhouse/lib/UI/screens/home_page.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:my_greenhouse/constants.dart';
+import 'package:my_greenhouse/data/sensor_data.dart';
+import 'package:my_greenhouse/data/thingspeak_service.dart';
 import 'package:my_greenhouse/models/sensors_box.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,14 +16,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //List Of devices and Sensors
+  final ThingspeakService thingSpeakService = ThingspeakService(
+    channelID: '2562505', // Replace with your ThingSpeak Channel ID
+    readAPIkey: 'WBTWK03HLNA3N339', // Replace with your ThingSpeak Read API Key
 
-  List myDevices = [
-    ['Temperature', 'assets/images/temperature.png', '45%' ],
-    ['Humidity', 'assets/images/humidity.png', '75%'],
-    ['Water Level', 'assets/images/water-tanks.png', '50%'],
-    ['Soil Moisture', 'assets/images/Soil.png', '30%'],
-  ];
+  );
+
+  late Future<SensorData?> sensorDataFuture;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    sensorDataFuture = thingSpeakService.fetchData();
+    _timer = Timer.periodic(const Duration(minutes: 1), (timer) {
+      setState(() {
+        sensorDataFuture = thingSpeakService.fetchData();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,7 +80,7 @@ class _HomePageState extends State<HomePage> {
                                           fontWeight: FontWeight.w500,
                                         ),
                                         speed:
-                                            const Duration(milliseconds: 100))
+                                        const Duration(milliseconds: 100))
                                   ],
                                   totalRepeatCount: 2,
                                   pause: const Duration(milliseconds: 1000),
@@ -123,29 +146,49 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                       const SizedBox(height: 10,),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.6,
-                        child: GridView.builder(
-                          itemCount: myDevices.length,
-                          physics: const NeverScrollableScrollPhysics(),
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  crossAxisSpacing: 8,
-                                  childAspectRatio: 11/10
-                              ),
-                          itemBuilder: (context, index) {
-                            return  Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: SensorBox(
-                                sensor: myDevices[index][0],
-                                iconPath: myDevices[index][1],
-                                status: myDevices[index][2],
+                      FutureBuilder<SensorData?>(
+                        future: sensorDataFuture,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return const Center(child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            return Center(child: Text('Error: ${snapshot.error}'));
+                          } else if (!snapshot.hasData || snapshot.data == null) {
+                            return const Center(child: Text('No data available'));
+                          } else {
+                            final sensorData = snapshot.data!;
+                            List myDevices = [
+                              ['Temperature', 'assets/images/temperature.png', '${sensorData.temperature}°C'],
+                              ['Humidity', 'assets/images/humidity.png', '${sensorData.humidity}%'],
+                              ['Water Level', 'assets/images/water-tanks.png', '${sensorData.waterLevel}%'],
+                              ['Soil Moisture', 'assets/images/Soil.png', '${sensorData.soilMoisture}%'],
+                            ];
+                            return SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.6,
+                              child: GridView.builder(
+                                itemCount: myDevices.length,
+                                physics: const NeverScrollableScrollPhysics(),
+                                gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    crossAxisSpacing: 8,
+                                    childAspectRatio: 11/10
+                                ),
+                                itemBuilder: (context, index) {
+                                  return  Padding(
+                                    padding: const EdgeInsets.all(1.0),
+                                    child: SensorBox(
+                                      sensor: myDevices[index][0],
+                                      iconPath: myDevices[index][1],
+                                      status: myDevices[index][2],
+                                    ),
+                                  );
+                                },
                               ),
                             );
-                          },
-                        ),
-                      )
+                          }
+                        },
+                      ),
                     ],
                   ),
                 ),
--- my_greenhouse/lib/main.dart
+++ my_greenhouse/lib/main.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:my_greenhouse/UI/onboardingscreen.dart';
 
 void main() {
-  // ignore: prefer_const_constructors
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'onboarding Screen',
+      title: 'Onboarding Screen',
       home: OnboardingScreen(),
       debugShowCheckedModeBanner: false,
     );
