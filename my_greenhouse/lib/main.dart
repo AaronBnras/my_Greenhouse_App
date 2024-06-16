acc9bb328ff74ddf7d575beb15da44409cd560ee
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-06-16 13:31:04 +0300

    Initial update

--- my_greenhouse/lib/main.dart
+++ my_greenhouse/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_greenhouse/UI/onboardingscreen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
