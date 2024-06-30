e4390bbb63a824932bc66ad70e410c1664934238
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-06-30 13:23:58 +0300

    Initial update

--- my_greenhouse/lib/UI/screens/profile.dart
+++ my_greenhouse/lib/UI/screens/profile.dart
@@ -1,9 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_greenhouse/UI/screens/widgets/custom_textfield.dart';
 import 'package:my_greenhouse/UI/screens/widgets/profile_widget.dart';
 import 'package:my_greenhouse/constants.dart';
+import 'package:my_greenhouse/services/firebase_authentication.dart';
+import 'package:my_greenhouse/UI/screens/sign_in.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  User? _user;
+  String _displayName = '';
+  String _email = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    _user = FirebaseAuth.instance.currentUser;
+    setState(() {
+      _displayName = _user?.displayName ?? 'No Name';
+      _email = _user?.email ?? 'No Email';
+    });
+  }
+
+  void _signOut(BuildContext context) async {
+    final FirebaseAuthentication _auth = FirebaseAuthentication();
+
+    try {
+      await _auth.signOut();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SignIn()),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error signing out. Please try again.')),
+      );
+    }
+  }
+
+  void _changePassword(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final TextEditingController _currentPasswordController = TextEditingController();
+        final TextEditingController _newPasswordController = TextEditingController();
+        final TextEditingController _confirmPasswordController = TextEditingController();
+        // final FirebaseAuthentication _auth = FirebaseAuthentication();
+
+        return AlertDialog(
+          title: Text('Change Password'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                customtextfield(
+                  obscureText: true,
+                  hintText: 'Current Password',
+                  icon: Icons.lock,
+                  controller: _currentPasswordController,
+                ),
+                customtextfield(
+                  obscureText: true,
+                  hintText: 'New Password',
+                  icon: Icons.lock,
+                  controller: _newPasswordController,
+                ),
+                customtextfield(
+                  obscureText: true,
+                  hintText: 'Confirm New Password',
+                  icon: Icons.lock,
+                  controller: _confirmPasswordController,
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Submit'),
+              onPressed: () async {
+                final currentPassword = _currentPasswordController.text.trim();
+                final newPassword = _newPasswordController.text.trim();
+                final confirmPassword = _confirmPasswordController.text.trim();
+
+                if (newPassword != confirmPassword) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('New passwords do not match.')),
+                  );
+                  return;
+                }
+
+                try {
+                  User? user = FirebaseAuth.instance.currentUser;
+                  if (user != null) {
+                    final cred = EmailAuthProvider.credential(
+                      email: user.email!,
+                      password: currentPassword,
+                    );
+
+                    await user.reauthenticateWithCredential(cred);
+                    await user.updatePassword(newPassword);
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Password successfully updated.')),
+                    );
+                    Navigator.of(context).pop();
+                  }
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Error: ${e.toString()}')),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +168,7 @@ class ProfilePage extends StatelessWidget {
                   child: Row(
                     children: [
                       Text(
-                        'Aaron Maeda',
+                        _displayName,
                         style: TextStyle(
                           color: Constants.blackColor,
                           fontSize: 16,
@@ -53,7 +181,7 @@ class ProfilePage extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  'aaronmaeda@gmail.com',
+                  _email,
                   style: TextStyle(
                     color: Constants.blackColor.withOpacity(.3),
                   ),
@@ -64,32 +192,19 @@ class ProfilePage extends StatelessWidget {
                 SizedBox(
                   height: size.height * .7,
                   width: size.width,
-                  child: const Column(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      // ProfileWidget(
-                      //   icon: Icons.person,
-                      //   title: 'My Profile',
-                      // ),
                       ProfileWidget(
                         icon: Icons.settings,
                         title: 'Settings',
+                        onTap: () => _changePassword(context),
                       ),
-                      // ProfileWidget(
-                      //   icon: Icons.notifications,
-                      //   title: 'Notifications',
-                      // ),
-                      // ProfileWidget(
-                      //   icon: Icons.chat,
-                      //   title: 'FAQs',
-                      // ),
-                      // ProfileWidget(
-                      //   icon: Icons.share,
-                      //   title: 'Share',
-                      // ),
                       ProfileWidget(
                         icon: Icons.logout,
                         title: 'Log Out',
+                        onTap: () => _signOut(context),
+                        showArrow: false,
                       ),
                     ],
                   ),
--- my_greenhouse/lib/UI/screens/widgets/profile_widget.dart
+++ my_greenhouse/lib/UI/screens/widgets/profile_widget.dart
@@ -4,45 +4,54 @@ import 'package:my_greenhouse/constants.dart';
 class ProfileWidget extends StatelessWidget {
   final IconData icon;
   final String title;
+  final VoidCallback? onTap;
+  final bool showArrow;
+
   const ProfileWidget({
     super.key,
     required this.icon,
     required this.title,
+    required this.onTap,
+    this.showArrow = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 18),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Icon(
-                icon,
-                color: Constants.blackColor.withOpacity(.5),
-                size: 24,
-              ),
-              const SizedBox(
-                width: 16,
-              ),
-              Text(
-                title,
-                style: TextStyle(
-                  color: Constants.blackColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 18),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  icon,
+                  color: Constants.blackColor.withOpacity(.5),
+                  size: 24,
                 ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Constants.blackColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            if (showArrow)
+              Icon(
+                Icons.arrow_forward_ios,
+                color: Constants.blackColor.withOpacity(.4),
+                size: 16,
               ),
-            ],
-          ),
-          Icon(
-            Icons.arrow_forward_ios,
-            color: Constants.blackColor.withOpacity(.4),
-            size: 16,
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
