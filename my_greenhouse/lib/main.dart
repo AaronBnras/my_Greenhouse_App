4fd2162858c6ee8334b058481bbc0d98e1bf7f3d
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-07-11 20:02:14 +0300

    Initial updates

--- my_greenhouse/lib/main.dart
+++ my_greenhouse/lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_greenhouse/UI/onboardingscreen.dart';
+import 'package:my_greenhouse/services/notification_service.dart';
 import 'firebase_options.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  if (kDebugMode) {
+    print("Handling a background message: ${message.messageId}");
+  }
+}
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await NotificationService().init();
   runApp(const MyApp());
 }
 
