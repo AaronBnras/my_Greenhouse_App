01d71304f873fa2fb59c6d279d351b59dd71c629
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-07-12 03:40:03 +0300

    Initial updates

--- my_greenhouse/lib/services/notification_service.dart
+++ my_greenhouse/lib/services/notification_service.dart
@@ -1,10 +1,23 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:my_greenhouse/data/firebase_service.dart';
+import 'dart:async';
 
 class NotificationService {
   final FirebaseMessaging _fcm = FirebaseMessaging.instance;
   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  // Variables to track the last notification time and device states
+  DateTime? _lastTemperatureNotification;
+  DateTime? _lastHumidityNotification;
+  DateTime? _lastSoilMoistureNotification;
+  DateTime? _lastWaterLevelNotification;
+  bool? _lastFanStatus;
+  bool? _lastPumpStatus;
+
+  // Constant for the cool-down period
+  static const Duration notificationCooldown = Duration(minutes: 15);
 
   Future<void> init() async {
     await _fcm.requestPermission();
@@ -45,30 +58,86 @@ class NotificationService {
   void listenToSensorData() {
     FirebaseService().sensorDataStream.listen((sensorData) {
       if (sensorData != null) {
-        if (sensorData.temperatureC > 32) {
-          sendNotification('High Temperature', 'Temperature is above 32°C');
+        if (kDebugMode) {
+          print('Received sensor data: $sensorData');
         }
-        if (sensorData.humidity > 70) {
-          sendNotification('High Humidity', 'Humidity is above 70%');
-        }
-        if (sensorData.soilMoisture < 30) {
-          sendNotification('Low Soil Moisture', 'Soil moisture is below 30%');
-        }
-        if (sensorData.waterLevel < 30) {
-          sendNotification('Low Water Level', 'Water level is below 30%');
-        }
-        if (sensorData.fanStatus) {
-          sendNotification('Fan Status', 'Fan is turned on');
-        } else {
-          sendNotification('Fan Status', 'Fan is turned off');
-        }
-        if (sensorData.pumpStatus) {
-          sendNotification('Water Pump Status', 'Water pump is turned on');
-        } else {
-          sendNotification('Water Pump Status', 'Water pump is turned off');
+
+        _checkAndNotifyTemperature(sensorData.temperatureC);
+        _checkAndNotifyHumidity(sensorData.humidity);
+        _checkAndNotifySoilMoisture(sensorData.soilMoisture);
+        _checkAndNotifyWaterLevel(sensorData.waterLevel);
+        _checkAndNotifyFanStatus(sensorData.fanStatus);
+        _checkAndNotifyPumpStatus(sensorData.pumpStatus);
+      } else {
+        if (kDebugMode) {
+          print('Received null sensor data');
         }
       }
     });
+  }
+
+  void _checkAndNotifyTemperature(double temperature) {
+    if (temperature > 32 && (_lastTemperatureNotification == null ||
+        DateTime.now().difference(_lastTemperatureNotification!) > notificationCooldown)) {
+      sendNotification('High Temperature', 'Temperature is above 32°C');
+      _lastTemperatureNotification = DateTime.now();
+      if (kDebugMode) {
+        print('Temperature condition met, sending notification');
+      }
+    }
+  }
+
+  void _checkAndNotifyHumidity(double humidity) {
+    if (humidity > 70 && (_lastHumidityNotification == null ||
+        DateTime.now().difference(_lastHumidityNotification!) > notificationCooldown)) {
+      sendNotification('High Humidity', 'Humidity is above 70%');
+      _lastHumidityNotification = DateTime.now();
+      if (kDebugMode) {
+        print('Humidity condition met, sending notification');
+      }
+    }
+  }
+
+  void _checkAndNotifySoilMoisture(double soilMoisture) {
+    if (soilMoisture < 30 && (_lastSoilMoistureNotification == null ||
+        DateTime.now().difference(_lastSoilMoistureNotification!) > notificationCooldown)) {
+      sendNotification('Low Soil Moisture', 'Soil moisture is below 30%');
+      _lastSoilMoistureNotification = DateTime.now();
+      if (kDebugMode) {
+        print('Soil moisture condition met, sending notification');
+      }
+    }
+  }
+
+  void _checkAndNotifyWaterLevel(double waterLevel) {
+    if (waterLevel < 30 && (_lastWaterLevelNotification == null ||
+        DateTime.now().difference(_lastWaterLevelNotification!) > notificationCooldown)) {
+      sendNotification('Low Water Level', 'Water level is below 30%');
+      _lastWaterLevelNotification = DateTime.now();
+      if (kDebugMode) {
+        print('Water level condition met, sending notification');
+      }
+    }
+  }
+
+  void _checkAndNotifyFanStatus(bool fanStatus) {
+    if (_lastFanStatus != fanStatus) {
+      sendNotification('Fan Status', fanStatus ? 'Fan is turned on' : 'Fan is turned off');
+      _lastFanStatus = fanStatus;
+      if (kDebugMode) {
+        print('Fan status changed, sending notification');
+      }
+    }
+  }
+
+  void _checkAndNotifyPumpStatus(bool pumpStatus) {
+    if (_lastPumpStatus != pumpStatus) {
+      sendNotification('Water Pump Status', pumpStatus ? 'Water pump is turned on' : 'Water pump is turned off');
+      _lastPumpStatus = pumpStatus;
+      if (kDebugMode) {
+        print('Pump status changed, sending notification');
+      }
+    }
   }
 
   Future<void> sendNotification(String title, String body) async {
